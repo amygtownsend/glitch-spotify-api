4f3178fc53b2252e18536366eefe71fb7bafce19
Glitch (amygtownsend-glitch-spotify-api) <none>
2019-02-13 03:39:31 +0000

    Complete step 2 in Orange

--- public/client.js
+++ public/client.js
@@ -52,7 +52,7 @@ $(function() {
     console.groupEnd();
     
     // The audio features we want to show
-    var keys = ["danceability", "energy", "acousticness"]
+    var keys = ["danceability", "energy", "acousticness", "loudness", "speechiness", "tempo"]
     
     // Display the audio features
     keys.map(function(key, i) {
